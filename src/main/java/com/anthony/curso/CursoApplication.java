184346f0371e49eaf806313428c769131c6726e1
Anthony Xavier <132829557+Anthony-Xavier@users.noreply.github.com>
2025-05-07 19:11:30 -0300

    primeiro commit

---
+++ src/main/java/com/anthony/curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
