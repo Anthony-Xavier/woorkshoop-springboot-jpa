d8e8d3472cad470f3d3636668ea050abfa844d2f
Anthony Xavier <132829557+Anthony-Xavier@users.noreply.github.com>
2025-05-08 13:05:43 -0300

    User and resource

---
+++ src/main/java/com/anthony/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.anthony.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anthony.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Anthony", "Anthony@gmail.com", "9999", "12455");
+        return ResponseEntity.ok().body(u);
+    }
+}
